ec7b39f891b5d25c3aa3eb08181d5e682f154b1b
YathrebShaher <yathrebshaher@gmail.com>
2021-08-13 18:06:40 +0200

    install back ground service package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,47 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:questions_task/app/pages/add_question.page.dart';
 
+import 'package:flutter_background_service/flutter_background_service.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterBackgroundService.initialize(onStart);
   runApp(MyApp());
+}
+
+void onStart() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final service = FlutterBackgroundService();
+  service.onDataReceived.listen((event) {
+    if (event!["action"] == "setAsForeground") {
+      service.setForegroundMode(true);
+      return;
+    }
+
+    if (event["action"] == "setAsBackground") {
+      service.setForegroundMode(false);
+    }
+
+    if (event["action"] == "stopService") {
+      service.stopBackgroundService();
+    }
+  });
+
+  // bring to foreground
+  service.setForegroundMode(true);
+  Timer.periodic(Duration(seconds: 1), (timer) async {
+    if (!(await service.isServiceRunning())) timer.cancel();
+    service.setNotificationInfo(
+      title: "My App Service",
+      content: "Updated at ${DateTime.now()}",
+    );
+
+    service.sendData(
+      {"current_date": DateTime.now().toIso8601String()},
+    );
+  });
 }
 
 class MyApp extends StatelessWidget {
